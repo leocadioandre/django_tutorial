32401c49bcfe34d707820e9bd1c2bb9b6844a70d
Andre Leocadio <andreleocadio4@gmail.com>
2020-10-14 18:15:46 -0300

    UpdateView

--- blog/models.py
+++ blog/models.py
@@ -34,6 +34,9 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('post_detail', args=[self.slug])
 
+    def get_absolute_url_update(self):
+        return reverse('post_edit', args=[self.pk])
+
 #alterar a ordem que é exibida as alterações - se remover o sinal de - volta a forma de antes
 
     class Meta:
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView
+from django.views.generic.edit import CreateView
 from . models import Post
 
 # Create your views here. Controla as listagem dos dados e as views
@@ -12,4 +13,18 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
-    context_object_name = 'custom'
+    #context_object_name = 'custom'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post_new.html'
+    fields = ('autor','titulo','conteudo')
+
+""" Poderia indicar no campo fields apenas os campos necessarios, sendo ou todos 
+feild = '__all__'
+"""
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'blog/post_new.html'
+    fields = ('titulo', 'conteudo')
+
